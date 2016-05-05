767d4338982a84c5e74a470561d5687e2d2a0b32
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-05 09:26:52 -0500

    Adds placeholder image for missing images and updates model validations for it

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
 
-  has_attached_file :avatar, :styles => { :medium => "400x400#", :thumb => "100x100#" }, :default_url => "images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "400x400#", :thumb => "100x100#" }, :default_url => "placeholder_:style.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
