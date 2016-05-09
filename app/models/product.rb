b5b0dae65069cd565a76210eac126fdee11283f7
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-09 09:10:12 -0500

    Adds storefront controller and updates index view with product list and Bootstrap modal

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
 
-  has_attached_file :avatar, :styles => { :medium => "400x400#", :thumb => "100x100#" }, :default_url => "placeholder_:style.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "placeholder_:style.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates_presence_of :name, :price, :quantity, :description, :brand, :category
+  validates_numericality_of :price, greater_than_or_equal_to: 0.01
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
 end
