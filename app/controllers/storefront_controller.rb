d19285d6985cf788786fab0d64c7f5b3b816b213
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-09 10:25:28 -0500

    Adds Search form to storefront index

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,10 +1,18 @@
 class StorefrontController < ApplicationController
   def index
-  	if params[:category_id]
+  	#TODO: Update notices to look Bootstrap
+  	if params[:search]
+  		@products = Product.search_by_name_or_description(params[:search]).includes(:brand)
+  		flash.now[:notice] = "Showing results for \"#{params[:search]}\":"
+  		if @products.empty?
+  			flash.now[:notice] = "No result found for \"#{params[:search]}\", showing all products."
+  			@products = Product.includes(:brand)
+  		end
+  	elsif params[:category_id]
   		@category = Category.find(params[:category_id])
-  		@products = Product.where(category_id: @category.id)
+  		@products = Product.where(category_id: @category.id).includes(:brand)
   	else
-  		@products = Product.all
+  		@products = Product.includes(:brand)
   	end
   end
 
