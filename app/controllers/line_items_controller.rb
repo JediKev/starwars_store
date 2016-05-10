fa894e7834c5ecf1fb614f99e9cc883f09102287
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-10 10:11:22 -0500

    Moves cart destroy from model to controller (oops) and includes currentcart in carts controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
 	include CurrentCart
 	before_action :set_cart
+	
 	def create
 		product = Product.find(params[:product_id])
 		@line_item = @cart.line_items.build(product: product, price: product.price)
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,17 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
 
-	def destroy
-		if @cart.destroy
-			session[:cart_id] = nil
-			flash[:notice] = "Cart was successfully deleted."
-			redirect_to shop_path
-		else
-			flash.now[:alert] = "Could not delete cart."
-			redirect_to edit_cart_path
-		end
-	end
-
 	def subtotal
 		# SQL Version of summing, faster than Ruby
 		line_items.select("SUM(quantity * price) AS sum")[0].sum
