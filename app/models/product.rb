1da6e8e30d4dfe5e332631c2b527ca52707e4574
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-10 08:57:24 -0500

    Creates cart and lineitem models and sets relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "placeholder_:style.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
@@ -12,4 +14,16 @@ class Product < ActiveRecord::Base
   def self.search_by_name_or_description(string)
   	where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%")
   end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items Present')
+        return false
+      end
+    end
+  end
+
 end
