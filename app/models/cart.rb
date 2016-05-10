12bf9ae5731955734d917081fecb4e2de8381893
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-10 09:38:53 -0500

    Sets subtotal and total price methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,11 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def subtotal
+		# SQL Version of summing, faster than Ruby
+		line_items.select("SUM(quantity * price) AS sum")[0].sum
+		
+		# Ruby Version of summing 
+		#line_items.to_a.sum{ |item| item.total }
+	end
 end
