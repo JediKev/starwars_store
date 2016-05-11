47d725a5e00bdb6ac35dc435eb2ac80aa8e5471c
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-11 08:43:57 -0500

    Fixes minor things (forgot what, oops)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,20 @@ class Cart < ActiveRecord::Base
 		# Ruby Version of summing 
 		#line_items.to_a.sum{ |item| item.total }
 	end
+
+	def add_product(product_id)
+		product = Product.find(product_id)
+		
+		# Find out if the item is already in cart. If it doesn't exist its value is nil.
+		current_item = line_items.find_by(product_id: product_id)
+
+		# If current item does exist, increase quantity. If not, build new line item.
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product: product, price: product.price)
+		end
+		current_item
+	end
+
 end
